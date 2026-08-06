dddd39ed51d7c1fe8b96f2480960670b368e2ef3
mohamedlemnawarmaaroufi-lgtm <mohamed.lemnawar.maaroufi@gmail.com>
2026-08-06 19:57:48 +0100

    Update print statements for UI readiness and loading

--- script.lua
+++ script.lua
@@ -652,7 +652,7 @@ function launchMainHub()
     tw(closeBtn, {BackgroundTransparency = 0.3, TextTransparency = 0}, 0.4)
     task.wait(0.25); tw(divLine, {BackgroundTransparency = 0.4}, 0.4)
 
-    print("[MH] Premium UI — Ready!")
+    print("[MOH] Premium itashi UI — Ready!")
 end
 
 -- ==================== ACTIVATE PROTECTION ====================
@@ -662,5 +662,5 @@ Ghost:AntiKickShield()
 Ghost:PlayerGuiShield()
 Ghost:AntiTeleportDetect()
 
-print("MH HUB VIP - LOADED")
+print("MOH-LOADED")
 
